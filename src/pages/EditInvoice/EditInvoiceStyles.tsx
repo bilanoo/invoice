f2f4f6d426c472d2e1528d056097e8d2c96075b0
bilanoo <blalkhan94@gmail.com>
2023-09-15 12:02:05 +0100

    removed back to home page styles from edit invoice page.

--- src/pages/EditInvoice/EditInvoiceStyles.tsx
+++ src/pages/EditInvoice/EditInvoiceStyles.tsx
@@ -1,26 +1,4 @@
-import { Box, Button, styled } from "@mui/material";
-
-export const PreviousPageContainer = styled(Box)(() => ({
-  "&": {
-    margin: "33px auto 31px 24px",
-  },
-}));
-
-export const PreviousPageButton = styled(Button)(({ theme }) => ({
-  "&": {
-    alignItems: "center",
-    fontSize: "0.938rem",
-    fontWeight: 700,
-    fontFamily: "inherit",
-    color: theme.palette.text.primary,
-    textTransform: "none",
-    borderRadius: "6px",
-  },
-  ".MuiButton-startIcon": {
-    width: "14px",
-    height: "14px",
-  },
-}));
+import { Box, styled } from "@mui/material";
 
 export const NavBar = styled(Box)(() => ({
   "&": {
@@ -50,6 +28,7 @@ export const ModifyInvoiceActionContainer = styled(Box)(({ theme }) => ({
     backgroundColor: theme.customPalette.invoiceBackgroundColor,
     justifyContent: "center",
     alignItems: "center",
+    marginTop: "auto",
   },
 
   "@media only screen and (min-width: 768px)": {
