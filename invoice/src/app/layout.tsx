6464de7ddf208ec4af9437a0745b833a0c3682ef
Blal <blalkhan94@gmail.com>
2025-04-28 18:22:21 +0100

    feat: implemented the header with the theme switcher

--- invoice/src/app/layout.tsx
+++ invoice/src/app/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Provider } from "react-redux";
 import { LocalizationProvider } from "@mui/x-date-pickers";
@@ -9,16 +8,6 @@ import { store } from "@/store/store";
 
 import ClientThemeProvider from "@/theme/ClientThemeProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -26,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <Provider store={store}>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <ClientThemeProvider>{children}</ClientThemeProvider>
--- invoice/src/app/page.tsx
+++ invoice/src/app/page.tsx
@@ -1,9 +1,18 @@
 import { Metadata } from "next";
+import { Container, InvoiceContainer } from "./HomepageStyles";
+import { Header } from "./_components/Header/Header";
 
 export const metadata: Metadata = {
   title: "Invoice App",
   description: "Create, Read, Update, Delete invoices",
 };
 export default function Home() {
-  return <p>hi</p>;
+  return (
+    <Container>
+      <Header />
+      <InvoiceContainer>
+        <h3>num of invoices</h3>
+      </InvoiceContainer>
+    </Container>
+  );
 }
