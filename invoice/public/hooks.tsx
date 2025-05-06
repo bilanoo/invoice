b3fae0118e18aafc582b4a034313b7a401f9f866
Blal <blalkhan94@gmail.com>
2025-05-06 16:09:42 +0100

    refactor: removed old app

--- invoice/public/hooks.tsx
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { AppdDispatch, RootState } from "./store";
-
-export const useAppDispatch: () => AppdDispatch = useDispatch;
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
