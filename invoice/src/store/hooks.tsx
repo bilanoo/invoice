db4bbb73e96b928627e3c295a1f52a76dbfe76ee
Blal <blalkhan94@gmail.com>
2025-05-06 16:17:12 +0100

    refactor: deleted invoice foflder

--- invoice/src/store/hooks.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { AppdDispatch, RootState } from "./store";
-
-export const useAppDispatch: () => AppdDispatch = useDispatch;
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
