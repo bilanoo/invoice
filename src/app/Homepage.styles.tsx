1bfe4478ab9acc482947ffbf7e1c2d06f892ba94
bilanoo <blalkhan94@gmail.com>
2025-05-18 17:57:56 +0100

    bug: fixed issue with container

--- src/app/Homepage.styles.tsx
+++ src/app/Homepage.styles.tsx
@@ -7,7 +7,6 @@ export const Container = styled(Box)(({ theme }) => ({
     display: "flex",
     flexDirection: "column",
     width: "100%",
-    height: "100%",
   },
   "@media only screen and (min-width: 483px) ": {
     "&": {
